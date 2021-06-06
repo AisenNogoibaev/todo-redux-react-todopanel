5e78f4b646013f69ef356ce5079ee6115bf341fa
Aisen Nogoibaev <aisennogoibaev999@gmail.com>
2021-06-06 18:59:19 +0600

    add animation ali

--- src/components/TodoPanel/index.jsx
+++ src/components/TodoPanel/index.jsx
@@ -3,7 +3,6 @@ import ali from './images/ali.jpg'
 import { Link} from 'react-router-dom'
 import { motion } from "framer-motion"
 import { useSelector} from "react-redux";
-
 export const TodoPanel = ({todo, id}) => {
 
   const todos = useSelector((state) => state);
@@ -17,7 +16,8 @@ console.log(todos)
             transition={{ duration: 0.5 }}>You</motion.h1>
         </Link>
 
-                <img width="350px" src={ali} alt="" />
+                <motion.img   whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }} width="350px" src={ali} alt="" />
             </div>
             <div className="exp-scale">   
                 <h3>You</h3>
